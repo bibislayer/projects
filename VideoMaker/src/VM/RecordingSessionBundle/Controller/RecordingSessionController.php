1519a4cc22c04bd324ad28810ceccd610dc4e5b9
bibislayer <adthev@gmail.com>
2014-04-29 21:25:54 +0200

    img user bdd

--- VideoMaker/src/VM/RecordingSessionBundle/Controller/RecordingSessionController.php
+++ VideoMaker/src/VM/RecordingSessionBundle/Controller/RecordingSessionController.php
@@ -3,7 +3,6 @@
 namespace VM\RecordingSessionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use VM\RecordingSessionBundle\Entity\RecordingSessionUser;
 use VM\RecordingSessionBundle\Form\RecordingSessionUserType;
 
@@ -28,39 +27,39 @@ class RecordingSessionController extends Controller {
         $user = $this->get('security.context')->getToken()->getUser();
         $recording_session = $this->get('recording_session_repository')->getElements(array('by_slug' => $slug_sess, 'action' => 'one'));
         if ($request->getMethod() == 'POST') {
-                $session_user = $this->get('recording_session_user_repository')->getElements(array('by_id' => $session->get('session_user'), 'action' => 'one'));
-                $em = $this->getDoctrine()->getManager();
-                $session_user->setFilename($request->get('filename'));
-                $em->persist($session_user);
-                $em->flush();
-                return $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
+            $session_user = $this->get('recording_session_user_repository')->getElements(array('by_id' => $session->get('session_user'), 'action' => 'one'));
+            $em = $this->getDoctrine()->getManager();
+            $session_user->setFilename($request->get('filename'));
+            $em->persist($session_user);
+            $em->flush();
+            return $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
         }
         return $this->render('VMRecordingSessionBundle:Default:show.html.twig', array('recordingSession' => $recording_session));
     }
-    
+
     public function moShowAction($slug_sess) {
         $request = $this->getRequest();
         $session = $request->getSession();
         $user = $this->get('security.context')->getToken()->getUser();
         $recording_session = $this->get('recording_session_repository')->getElements(array('by_slug' => $slug_sess, 'action' => 'one'));
         if ($request->getMethod() == 'POST') {
-                
+            
         }
         return $this->render('VMRecordingSessionBundle:Middle:show.html.twig', array('recordingSession' => $recording_session));
     }
-    
-    public function moSessionUserDeleteAction($slug_sess){
-         $request = $this->getRequest();
-         $em = $this->getDoctrine()->getManager();
-         $id = $request->get('id');
-         $session_user = $this->get('recording_session_user_repository')->getElements(array('by_id' => $id, 'action' => 'one'));
-         $em->remove($session_user);
-         $em->flush();
-         
-         echo $id;
-         exit;
+
+    public function moSessionUserDeleteAction($slug_sess) {
+        $request = $this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->get('id');
+        $session_user = $this->get('recording_session_user_repository')->getElements(array('by_id' => $id, 'action' => 'one'));
+        $em->remove($session_user);
+        $em->flush();
+
+        echo $id;
+        exit;
     }
-    
+
     public function moSessionDashboardAction($slug_sess) {
         $recording_session = $this->get('recording_session_repository')->getElements(array('by_slug' => $slug_sess, 'action' => 'one'));
         return $this->render('VMRecordingSessionBundle:Middle:session_dashboard.html.twig', array('recordingSession' => $recording_session));
@@ -71,14 +70,14 @@ class RecordingSessionController extends Controller {
         $session = $request->getSession();
         if ($session->get('session_user') != null) {
             $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
-        }     
-          
+        }
+
         $recording_session = $this->get('recording_session_repository')->getElements(array('by_slug' => $slug_sess, 'action' => 'one'));
         $session_user = new RecordingSessionUser();
         $session_user->setRecordingSession($recording_session);
 
         $form = $this->get('form.factory')->create(new RecordingSessionUserType(), $session_user);
-        
+
         if ($request->getMethod() == 'POST') {
             $form->handleRequest($request);
             if ($form->isValid()) {
@@ -92,26 +91,40 @@ class RecordingSessionController extends Controller {
         }
         return $this->render('VMRecordingSessionBundle:Default:login.html.twig', array('form' => $form->createView()));
     }
-    
+
     public function sessionProcessAction($slug_sess) {
         $request = $this->getRequest();
         $session = $request->getSession();
         if (!$session->get('session_user') || $session->get('session_user') == null) {
             return $this->redirect($this->generateUrl('session_login', array('slug_sess' => $slug_sess)));
-        }   
-          
+        }
+
         $session_user = $this->get('recording_session_repository')->getElements(array('by_id' => $session->get('session_user'), 'action' => 'one'));
 
         if ($request->getMethod() == 'POST') {
-            
-                $em = $this->getDoctrine()->getManager();
-                $session_user->setFilename($recording_session);
-                $em->persist($session_user);
-                $em->flush();
-                return $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
-            
+
+            $em = $this->getDoctrine()->getManager();
+            $session_user->setFilename($recording_session);
+            $em->persist($session_user);
+            $em->flush();
+            return $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
         }
         return $this->render('VMRecordingSessionBundle:Default:login.html.twig', array('form' => $form->createView()));
+    }
+
+    public function moAjaxSaveForm($slug_sess) {
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            print_r($request->request);
+            exit;
+            $recording_session = $this->get('recording_session_repository')->getElements(array('by_slug' => $slug_sess, 'action' => 'one'));
+            $em = $this->getDoctrine()->getManager();
+            
+            $recording_session->setFilename($recording_session);
+            $em->persist($session_user);
+            $em->flush();
+            return $this->redirect($this->generateUrl('fo_recording_session_show', array('slug_sess' => $slug_sess)));
+        }
     }
 
 }
