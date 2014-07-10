6942c477433fa1c881bbc9581ae4856862dbf02f
bibislayer <adthev@gmail.com>
2014-07-10 16:19:06 +0200

    webcam debug commit

--- VideoMaker/src/VM/RecordingSessionBundle/Controller/RecordingSessionController.php
+++ VideoMaker/src/VM/RecordingSessionBundle/Controller/RecordingSessionController.php
@@ -31,7 +31,7 @@ class RecordingSessionController extends Controller {
         $kernel = $this->get('kernel');
         $streamsPath = $kernel->getRootDir().'/../web/uploads/streams/';
         if ($request->getMethod() == 'POST') {
-        $cmd = 'ffmpeg -y -i '.$streamsPath.$request->get('filename').'.flv -s 640x480 -ar 44100 -pass 1 -b 1400k -r 30 -ab 128k -f avi '.$streamsPath.$request->get('filename').'.avi >> '.$kernel->getRootDir().'/logs/encoder.txt';
+        $cmd = 'ffmpeg -y -i '.$streamsPath.$request->get('filename').'.flv -s 640x480 -ar 44100 -b 1400k -r 30 -ab 128k -f avi '.$streamsPath.$request->get('filename').'.avi >> '.$kernel->getRootDir().'/logs/encoder.txt';
         //$cmd = 'touch '.$streamsPath.'test.txt';
         $process = new Process($cmd);
         $process->run();
