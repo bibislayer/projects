05a0fd5f029bf1bfdf6ca9e790d11fc11026e2fa
bibislayer <adthev@gmail.com>
2014-12-05 21:44:50 +0100

    debug nodejs

--- nodejs/media/public/js/main.js
+++ nodejs/media/public/js/main.js
@@ -39,7 +39,7 @@ $(function () {
     );
 
     $('#fileupload').fileupload('option', {
-        url: '//localhost:8080/files',
+        url: '/',
         // Enable image resizing, except for Android and Opera,
         // which actually support image resizing, but fail to
         // send Blob objects via XHR requests:
