0b46bb72951ded929d4a987e8f7ce0d9f54777a0
bibislayer <adthev@gmail.com>
2014-12-26 01:52:40 +0100

    debug

--- nodejs/media/app.js
+++ nodejs/media/app.js
@@ -140,7 +140,7 @@ function issueToken(user, done) {
     });
 }
 // Connect mongoose
-mongoose.connect('mongodb://localhost/media', {user: 'bibislayer', pass: '@nicktalope78@'});
+mongoose.connect('mongodb://localhost/media');
 // configure Express
 app.configure(function () {
     app.set('views', __dirname + '/views');
